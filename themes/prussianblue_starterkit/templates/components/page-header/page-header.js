d83aedb559554614a3730d0de60dd4930ce26424
Noel Rivas <noel@matrushka.com.mx>
2023-02-28 12:43:33 -0600

    Improved the styles and behavior of the page-header component, which now works reasonably well in desktop.

--- themes/prussianblue_starterkit/templates/components/page-header/page-header.js
+++ themes/prussianblue_starterkit/templates/components/page-header/page-header.js
@@ -1,14 +1,37 @@
-(function ($, Drupal) {
+(function (Drupal) {
   Drupal.behaviors.pb_main_menu = {
     attach: function attach(context) {
       const main_menu_links = once('main-menu', '.menu--main ul.menu > li.menu-item--expanded', context);
 
       main_menu_links.forEach((element) => {
+        let elementTimer;
+
+        element.setAttribute('aria-expanded', false);
+
         element.addEventListener("click", (event) => {
-          event.target.parentNode.classList.toggle('is-open');
+          element.classList.toggle('is-open');
+
+          if (event.target.parentNode.classList.contains('is-open')) {
+            element.setAttribute('aria-expanded', true);
+          }
+          else {
+            element.setAttribute('aria-expanded', false);
+          }
+
           event.preventDefault();
+        });
+
+        element.addEventListener('mouseover', (event) => {
+          element.classList.add('is-open');
+          clearTimeout(elementTimer);
+        });
+
+        element.addEventListener('mouseout', (event) => {
+          elementTimer = setTimeout((event) => {
+            document.querySelector('.menu-item--expanded.is-open').classList.remove('is-open');
+          }, 500);
         });
       });
     },
   };
-})(jQuery, Drupal);
+})(Drupal);
